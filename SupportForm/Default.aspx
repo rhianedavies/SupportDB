12d1a5c39ddefc2a0d9ca7c08293c9e95a4f1fe4
Rhian Davies <rhianedavies@outlook.com>
2018-06-19 10:21:58 +0100

    webforms :)

---
+++ SupportForm/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SupportForm._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div id="questions" runat="server"></div>
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+</asp:Content>
---
+++ SupportForm/Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.DynamicData;
+
+namespace SupportForm
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SupportDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True");
+            SqlDataAdapter daQ = new SqlDataAdapter("select * from questions order by questionno", conn);
+            SqlDataAdapter daAns = new SqlDataAdapter("SELECT * from MultipleChoiceAnswers", conn);
+            DataSet ds = new DataSet();
+            daQ.Fill(ds.Tables.Add("questions"));
+            daAns.Fill(ds.Tables.Add("answers"));
+            questions.InnerHtml += "<div class='content'>";
+            foreach (DataRow dr in ds.Tables["questions"].Rows)
+            {
+                questions.InnerHtml += "<div class='row'>";
+                questions.InnerHtml += "<div class='col-sm'>" + dr["questionno"] + ") " + dr["question"] + "</div>\n";
+                DataView dvA = ds.Tables["answers"].DefaultView;
+                dvA.RowFilter = "questionid = '" + dr["questionid"] + "'";
+                DataTable dtMCA = dvA.ToTable();
+                if (dtMCA.Rows.Count > 0)
+                {
+                    questions.InnerHtml += "<div class='col-sm'><select id='" + dr["questionid"] + "' name='" + dr["questionid"] + "' runat='server'>\n";
+                    foreach (DataRow drMCA in dtMCA.Rows)
+                    {
+                        questions.InnerHtml += "<option value='" + drMCA["multiplechoiceanswersid"] + "'>" + drMCA["answer"] + "</option>\n";
+                    }
+
+                    questions.InnerHtml += "<option selected>--SELECT--</option>\n";
+                    questions.InnerHtml += "</select></div>";
+                }
+                else
+                {
+                    questions.InnerHtml += "<div class='col-sm'><input type='text' id='" + dr["questionid"] + "' name='" + dr["questionid"] + "' runat='server'></div>\n";
+                }
+
+                questions.InnerHtml += "</div>\n";
+            }
+
+            questions.InnerHtml += "</div>\n";
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SupportDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True");
+            SqlDataAdapter daQ = new SqlDataAdapter("select * from questions order by questionno", conn);
+            DataSet ds = new DataSet();
+            daQ.Fill(ds);
+            SqlCommand cmdQuestionnaire = new SqlCommand("insert into questionnaire (questionnaireid) values (@questionnaireid)", conn);
+            var questionnaireId = Guid.NewGuid();
+            cmdQuestionnaire.Parameters.Add(new SqlParameter("@questionnaireid", questionnaireId));
+            conn.Open();
+            cmdQuestionnaire.ExecuteNonQuery();
+            conn.Close();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                var answer = Request.Form[dr["questionid"].ToString()];
+                var freeTextAnswersId = Guid.NewGuid();
+                if (Guid.TryParse(answer, out Guid aGuid) == false && answer != "--SELECT--")
+                {
+                    SqlCommand cmdFreeTextAnswer = new SqlCommand("insert into freetextanswers (freetextanswersid, " +
+                                                                  "questionid, answer) values (@freetextanswersid, " +
+                                                                  "@questionid, @answer)", conn);
+                    cmdFreeTextAnswer.Parameters.Add(new SqlParameter("@freetextanswersid", freeTextAnswersId));
+                    cmdFreeTextAnswer.Parameters.Add(new SqlParameter("@questionid", dr["questionid"].ToString()));
+                    cmdFreeTextAnswer.Parameters.Add(new SqlParameter("@answer", answer));
+                    conn.Open();
+                    cmdFreeTextAnswer.ExecuteNonQuery();
+                    conn.Close();
+                }
+                SqlCommand cmdAns = new SqlCommand("insert into answers (questionnaireid, questionid, multiplechoiceanswersid, " +
+                                                   "freetextanswersid) values (@questionnaireid, @questionid, " +
+                                                   "@multiplechoiceanswersid, @freetextanswersid)", conn);
+                cmdAns.Parameters.Add(new SqlParameter("@questionnaireid", questionnaireId));
+                cmdAns.Parameters.Add(new SqlParameter())
+            }
+        }
+    }
+}
